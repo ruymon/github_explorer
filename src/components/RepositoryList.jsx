c4f1a5091ad24f3f7c737f926f1188c162bcba16
ruymon <ruystfrancis@gmail.com>
2021-04-11 21:41:59 -0300

    :sparkles: Add API Integration and Display on Screen

--- src/components/RepositoryList.jsx
+++ src/components/RepositoryList.jsx
@@ -1,23 +1,25 @@
+import { useState, useEffect } from "react";
 import { RepositoryItem } from "./RespositoryItem";
 
 import '../styles/repositories.scss';
 
-const repository = {
-    name: "Bhaskalc",
-    description: "Calculator",
-    link: "http://github.com/ruymon/bhaskalc"
-}
-
 export function RepositoryList() {
+    const [repositories, setRepositories] = useState([]);
+
+    useEffect(() => {
+        fetch('http://api.github.com/users/ruymon/repos')
+           .then(response => response.json())
+           .then(data => setRepositories(data))
+    }, []);
+
     return (
         <section className="repository-list">
-            <h1>Lista da Repositórios</h1>
+            <h1>Lista de Repositórios</h1>
 
             <ul>
-                <RepositoryItem repository={repository} />
-                <RepositoryItem repository={repository} />
-                <RepositoryItem repository={repository} />
-                <RepositoryItem repository={repository} />
+                {repositories.map(repository => {
+                    return <RepositoryItem key={repository.id} repository={repository} />
+                })}
             </ul>
         </section>
     );
--- src/components/RespositoryItem.jsx
+++ src/components/RespositoryItem.jsx
@@ -1,11 +1,11 @@
 export function RepositoryItem(props) {
     return (
         <li>
-            <strong>{props.repository.name ?? 'Default'}</strong>
+            <strong>{props.repository.name}</strong>
             <p>{props.repository.description}</p>
 
-            <a href={props.repository.link}>
-                Acessar Respositórios
+            <a href={props.repository.html_url}>
+                Acessar Respositório
             </a>
         </li>
     );
